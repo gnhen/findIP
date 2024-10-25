5b6a064b593c4019a78f0ab7e4b65343913a3622
Grant Hendricks <gtnhen@gmail.com>
2024-10-24 23:45:17 -0400

    Added Copy option

--- ipv4-finder.py
+++ ipv4-finder.py
@@ -1,16 +1,21 @@
 import subprocess
+import pyperclip
 
 
 def get_wifi_ip():
     command = "powershell \"Get-NetIPAddress -InterfaceAlias Wi-Fi | Where-Object {$_.AddressFamily -eq 'IPv4'} | Select-Object -ExpandProperty IPAddress\""
     result = subprocess.run(command, capture_output=True, text=True, shell=True)
-    print(result.stdout.strip())
+    return result.stdout.strip()
 
 
 if __name__ == "__main__":
-    get_wifi_ip()
+    ip_address = get_wifi_ip()
+    print(ip_address)
 
     while True:
-        user_input = input("Press 'Q' to quit: ").strip().upper()
-        if user_input == "Q":
+        user_input = input("Press 'C' to copy, or 'Q' to quit: ").strip().upper()
+        if user_input == "C":
+            pyperclip.copy(ip_address)
+            print("IP address copied to clipboard.")
+        elif user_input == "Q":
             break
